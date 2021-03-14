b021e419c237ed489637fee291d22a0173446cc3
Potrashitel83 <71212646+Ilya837@users.noreply.github.com>
2021-03-14 13:42:16 +0300

    Update Date.cpp

--- Date/Date.cpp
+++ Date/Date.cpp
@@ -40,14 +40,25 @@ Date::Date(string date)
 {
 	int day = (date[0] - '0') * 10 + (date[1] - '0');
 	int month = (date[3] - '0') * 10 + (date[4] - '0');
-	for(int i = 6; date[i] != '\n';i++){
-	}
-
 	Year = 0;
-
-	for (int i = 6; date[i] != '\n'; i++) {
+	for(int i = 6; date[i] != '\0';i++){
 		Year = Year * 10 + (date[i] - '0');
 	}
+	assert(month <= 12 && month >= 0);
+	if (month != 2) {
+		assert(day <= Days[month] && day > 0);
+	}
+	else {
+		if (Year % 4 == 0 && Year % 1000 != 0) {
+			assert(day <= Days[month] + 1 && day > 0);
+		}
+		else {
+			assert(day <= Days[month] && day > 0);
+		}
+	}
+	Day = day;
+	Month = month;
+	int a = 0;
 }
 
 Date::~Date()
@@ -56,36 +67,53 @@ Date::~Date()
 
 Date Date::operator=(const Date& date)
 {
-	return Date();
+	Day = date.Day;
+	Month = date.Month;
+	Year = date.Year;
+	return *this;
 }
 
 Date Date::operator+(const int& days) const
 {
+
 	return Date();
 }
 
 bool Date::operator<(const Date& date)
 {
+	if (date.Year > Year) {
+		return true;
+	}
+	if (date.Month > Month) {
+		return true;
+	}
+	if (date.Day > Day) {
+		return true;
+	}
 	return false;
 }
 
 bool Date::operator>(const Date& date)
 {
-	return false;
+	bool a = *this < date ? false : true;
+	if (date.Year == Year && date.Month == Month && date.Day == Day) a = false;
+	return a;
 }
 
 bool Date::operator<=(const Date& date)
 {
-	return false;
+	
+	return *this > date ? false : true;
 }
 
 bool Date::operator>=(const Date& date)
 {
-	return false;
+	return *this < date ? false : true;
 }
 
 bool Date::operator==(const Date& date)
 {
+	if (date.Year == Year && date.Month == Month && date.Day == Day) return true;
 	return false;
 }
 
@@ -98,3 +126,4 @@ bool Date::operator==(const Date& date)
 //{
 //	// TODO: вставьте здесь оператор return
 //}
+
